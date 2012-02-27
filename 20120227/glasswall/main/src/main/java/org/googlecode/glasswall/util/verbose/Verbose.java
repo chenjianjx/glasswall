7a1c177bcca0bc7f099a913b8a8fa69dcd80e21f
chenjianjx@gmail.com <chenjianjx@gmail.com@ba2fb965-2415-44f1-193d-1bdcebb25034>
2012-02-27 15:27:30 +0000

    merge to trunk

---
+++ 20120227/glasswall/main/src/main/java/org/googlecode/glasswall/util/verbose/Verbose.java
@@ -0,0 +1,18 @@
+package org.googlecode.glasswall.util.verbose;
+
+/**
+ * do verbose for this tool itself
+ * 
+ * @author chenjianjx
+ * 
+ */
+public interface Verbose {
+	public static final String PROJECT_NAME = "Glasswall";
+	public void println(String msg);
+	public void println(Throwable e);
+	public void println(String msg, Throwable e);
+	
+ 
+}
+
+
